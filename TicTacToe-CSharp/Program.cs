b02b1d35cf79bb8fd83c96df5bbb8b4a89cca5da
Klitos Giannakopoulos <klitos@duckseason.mobi>
2015-03-29 21:01:34 +0300

    initial commit
    project and solution creation

---
+++ TicTacToe-CSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: klitos
+ * Date: 3/29/2015
+ * Time: 8:29 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace TicTacToe_CSharp
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
