951c68940ac17bd1cd51eabe894e75d8b7bce053
Klitos Giannakopoulos <klitos@duckseason.mobi>
2015-03-29 21:30:02 +0300

    draw the grid

--- TicTacToe-CSharp/GameBoardView.cs
+++ TicTacToe-CSharp/GameBoardView.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace TicTacToe_CSharp
@@ -15,9 +16,24 @@ namespace TicTacToe_CSharp
 	/// Description of GameBoardView.
 	/// </summary>
 	public class GameBoardView: Control
-	{
+	{		
 		public GameBoardView()
 		{
+		}
+		
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+			
+			int cellWidth = Size.Width / 3;
+			int cellHeight = Size.Height / 3;
+			
+			Pen pen = new Pen(ForeColor);
+			
+			e.Graphics.DrawLine(pen, cellWidth, 0, cellWidth, Size.Height);
+			e.Graphics.DrawLine(pen, cellWidth * 2, 0, cellWidth * 2, Size.Height);
+			e.Graphics.DrawLine(pen, 0, cellHeight, Size.Width, cellHeight);
+			e.Graphics.DrawLine(pen, 0, cellHeight * 2, Size.Width, cellHeight * 2);
 		}
 	}
 }
